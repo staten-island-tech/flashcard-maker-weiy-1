8501de1f452c25d128288dd3cc70a6f92dd7476d
weiy-1 <weiyuanyan1126@gmail.com>
2025-03-25 10:17:39 -0400

    wip again

--- flascar.py
+++ flascar.py
@@ -1,16 +1,37 @@
 import json
 
-class flashcaards:
-    def __init__(self, question, answer):
-        self.question = question
-        self.answer = answer
+class teacher:
+    def teacher_cards():
+        doned = False
+        madedcards = []
+        while doned == False:
+            isingthewanting = input("do u are want to do the make card thing (yes or no): ")
+            if isingthewanting == "yes":
+                wip here
+            questioning = input("enter question for a card (if u done, type done): ")
+            ansering = input("enter the answer to the question: ")
+            if questioning == "done":
+                doned = True
+                with open("FlashCards.json", "w") as file:
+                    json.dump(madedcards, file, indent=4)
 
-    def display(self):
-        print(self.question, self.answer)
+            
+
+
+class spudent:
+    def spudent_cards():
+        print("testing1")
+
 
 t_or_s = input("teacher mode or student mode (enter t or s): ")
 if t_or_s == "t":
     print("u are now in teacher mode")
-    input_ques = input("enter question: ")
-    input_ans = input("enter answer to the question: ")
+    teacher.teacher_cards()
+
+elif t_or_s == "s":
+    print("u are now in student mode")
+    spudent.spudent_cards()
+
+else:
+    print("ur input no working")
     
